df719d67472953d862965cb79b028139d83c296b
arjungautam1 <arjungautam5431@gmail.com>
2023-09-11 20:12:01 +0545

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One" },
+  ]);
+
+  const addTodo = (description, assigned) => {
+    const rowNumber =
+      todos.length > 0 ? todos[todos.length - 1].rowNumber + 1 : 1;
+
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-body">
+          <button
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+            className="btn btn-primary"
+          >
+            {showAddTodoForm ? "Close New Todo" : " New Todo"}
+          </button>
+
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
+
+          <div className="card-header mt-3">Your Todo's</div>
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+        </div>
+      </div>
     </div>
   );
 }
